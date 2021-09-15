acd123ba58669a34d3b6695f2b91560fc2eeef4b
xeny <44981148+1x6@users.noreply.github.com>
2021-09-15 17:30:17 +0100

    kek

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os, re
 
 ext = input('Enter the files\' extension: (e.g .mkv) ')
+directory = input('Enter the directory to look through: ')
 
-directory = os.getcwd()
 for filename in os.listdir(directory):
     if filename.endswith(ext):
         print(filename)
         new_fn = re.sub(ext,  '.3gp', filename)
-        os.popen(f'''ffmpeg -y -i "{filename}" -r 20 -s 352x288 -vb 400k -acodec aac -strict experimental -ac 2 -ab 128k "{new_fn}"''')
+        os.popen(f'''ffmpeg -y -i "{directory}/{filename}" -r 20 -s 352x288 -vb 400k -acodec aac -strict experimental -ac 2 -ab 128k "{directory}/{new_fn}"''')
     else:
         continue
